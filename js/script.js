15227c67e38c4a367a9fe6a1eb82c53ac05501f7
Ezequiel <ezequiel.domenech@gmail.com>
2019-04-14 23:30:43 -0300

    reescrito el script, algunos cambios en contacto y navegación y estilos

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 "use strict";
-let btnEnviar = document.querySelector(".btnEnviar");
+let btnEnviar = document.querySelector(".btnEnviar"); //boton de envio de form
+let valcap = document.querySelector(".validateCap"); //captcha ingresado, a validar
+let captcha = document.querySelector(".captcha"); //lugar donde se va a depositar el captcha
 btnEnviar.addEventListener("click", validate);
 
-let captcha = document.querySelector(".captcha");
-let valcap = document.querySelector(".validateCap");
-let numRandom = Math.floor((Math.random() * 300) + 1);
-captcha.innerHTML += numRandom;
+function generate (captcha){
+  let numRandom = Math.floor((Math.random() * 999) + 1);
+  captcha.innerHTML = numRandom; //aca se asigna el valor generado
+}
 
 function validate (e) {
-  let codigo  = captcha.innerHTML;
-  let validar = valcap.value;
+  e.preventDefault();
+  let formulario = document.querySelector(".contacto-form");
+  let codigo  = captcha.innerHTML; //tomo el valor que se genero y se asigno anteriormente
+  let validar = valcap.value; //tomo el valor del input para comparar
   if (codigo == validar) {
     alert("Datos ingresados correctamente");
+    formulario.submit();
   }
   else {
     alert("Codigo de validación erroneo");
+    generate(captcha);
   }
 }
+generate(captcha);
